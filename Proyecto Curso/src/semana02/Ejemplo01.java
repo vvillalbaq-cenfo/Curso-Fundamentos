2a281a56db17668cb646dcfd66e31b1b77fa9e13
vvillalbaq-cenfo <vvillalbaq@ucenfotec.ac.cr>
2025-05-22 18:55:50 -0600

    Add files via upload

---
+++ Proyecto Curso/src/semana02/Ejemplo01.java
@@ -0,0 +1,10 @@
+package semana02;
+
+import utilitarios.Utils;
+
+public class Ejemplo01 {
+    public static void main(String[] args) {
+      util.limpiarConsola();
+      system.out.println("Ahora si");
+    }
+} 
