0d9cfd00581290aa6e193c0817d7b24be698846c
vvillalbaq-cenfo <vvillalbaq@ucenfotec.ac.cr>
2025-05-27 03:36:28 +0000

    S02

---
+++ Proyecto Curso/src/ejerciciospropios/Ejercicio01.java
@@ -0,0 +1,30 @@
+package ejerciciospropios;
+import utilitarios.Utils;
+public class Ejercicio01 {
+    public static void main(String[] args) {
+      /*
+      Calcular el precio final de cualquier producto, si se sabe que la tasa de impuesto al valor 
+      agregado es de un 13 %. El algoritmo debe recibir el nombre del producto y el precio, y
+      debe imprimir el nombre del producto y el precio final. Para los efectos del ejercicio,
+      TODOS los productos pagan impuesto.
+      */ 
+        Utils.limpiarConsola();
+        System.out.println("Ejercicio 01: Calcular el precio final de un producto con IVA");
+        
+
+        String nombreProducto;
+        nombreProducto = Utils.leerString(("Ingrese el nombre del producto:"));
+        
+        Double precioProducto;
+        precioProducto = Utils.leerDoble(("Ingrese el precio del producto: "));
+
+        Double iva = 0.13;
+        Double calculoIVA = precioProducto * iva;
+        Double precioFinal = precioProducto + calculoIVA;
+        
+        System.out.println("Nombre del producto: " + nombreProducto);
+        System.out.println("Precio del producto: " + precioProducto);
+        System.out.println("Precio final con IVA: " + precioFinal);
+        System.out.println("Hecho por Victor Villalba Quintana");
+    }
+}
