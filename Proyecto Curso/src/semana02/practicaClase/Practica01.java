dfab707825c8a169fb41a77540774ad467ac20ee
vvillalbaq-cenfo <vvillalbaq@ucenfotec.ac.cr>
2025-05-28 02:29:42 +0000

    S02

--- Proyecto Curso/src/semana02/practicaClase/Practica01.java
+++ Proyecto Curso/src/semana02/practicaClase/Practica01.java
@@ -1,7 +1,30 @@
-package semana02.practicaClase;   
-   
-public class Practica01 {   
-  public static void main(String[] args) {   
-     
-}
+package semana02.practicaClase;
+
+import utilitarios.Utils;
+
+public class Practica01 {
+    public static double PRECIO_UNITARIO = 10;
+    public static double COSTO_UNITARIO = 5.5;
+
+    public static void main(String[] args) {
+        Utils.limpiarConsola();
+
+        int cantidadProducto = 0;
+        double utilidadUnitaria = 0.0, utilidadTotal = 0.0, utilidadHugo = 0.0, utilidadPaco = 0.0, utilidadLuis = 0.0;
+
+        // inputs
+        cantidadProducto = Utils.leerRangosEnteros("Digite la cantidad de limonadas: ", 1, 100);
+
+        // process
+        utilidadUnitaria = PRECIO_UNITARIO - COSTO_UNITARIO;
+        utilidadTotal = utilidadUnitaria * cantidadProducto;
+        utilidadHugo = utilidadTotal * 0.4;
+        utilidadLuis = utilidadTotal * 0.3;
+        utilidadPaco = utilidadTotal * 0.3;
+
+        // out
+        System.out.println("Hugo: " + utilidadHugo);
+        System.out.println("Luis: " + utilidadLuis);
+        System.out.println("Paco: " + utilidadPaco);
+    }
 }
