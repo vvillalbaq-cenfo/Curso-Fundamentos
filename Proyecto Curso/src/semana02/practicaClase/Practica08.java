be95172ce8a20d1fa6971b13aba38590f89948f5
vvillalbaq-cenfo <vvillalbaq@ucenfotec.ac.cr>
2025-05-28 15:10:33 +0000

    S02

--- Proyecto Curso/src/semana02/practicaClase/Practica08.java
+++ Proyecto Curso/src/semana02/practicaClase/Practica08.java
@@ -1,13 +1,14 @@
 package semana02.practicaClase;
+
 import utilitarios.Utils;
 
 public class Practica08 {
-     public static final double PORCENTAJE_IMPUESTO = 0.30;
+    public static final double PORCENTAJE_IMPUESTO = 0.30;
     public static final double GANANCIA_VENDEDOR = 0.10;
 
     public static void main(String[] args) {
         Utils.limpiarConsola();
-        //Entradas
+        // Entradas
         double costoUnitario = Utils.leerDoble("Ingresar precio del vehículo");
 
         // Proceso
